3ded3ddb1ca4f8cc270bb516a84f5d7cf9a9d4cc
Anshi Aggarwal <52320166+anshi-a2@users.noreply.github.com>
2025-04-26 23:36:34 +0530

    Create 13. Roman to Integer.cpp

---
+++ leetcode/13. Roman to Integer.cpp
@@ -0,0 +1,53 @@
+// PROBLEM :
+
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+  
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+  
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+
+// SOLUTION :
+
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10}, 
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (roman[s[i]] < roman[s[i + 1]]) {
+                res -= roman[s[i]];
+            } else {
+                res += roman[s[i]];
+            }
+        }
+
+        return res + roman[s[s.size() - 1]];  
+    }
+};
+
+  
