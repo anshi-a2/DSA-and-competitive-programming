cf856053d639057964180fc09a3e1730e5a21563
Anshi Aggarwal <52320166+anshi-a2@users.noreply.github.com>
2025-04-30 22:26:24 +0530

    Create 30. Substring with Concatenation of All Words.cpp

---
+++ leetcode/30. Substring with Concatenation of All Words.cpp
@@ -0,0 +1,86 @@
+// PROBLEM :
+
+
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+
+Output: [0,9]
+
+Explanation:
+
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+
+Example 2:
+
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+
+Output: []
+
+Explanation:
+
+There is no concatenated substring.
+
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+
+Output: [6,9,12]
+
+
+
+
+// SOLUTION :
+
+
+class Solution {
+public:
+    bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
+        for(int j=0; j<s.size(); j+=wordLen) {
+            string w = s.substr(j, wordLen);
+            if(wordCount.find(w) != wordCount.end()) {
+                if(--wordCount[w] == -1) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        int wordLen = words[0].size();
+        int sLen = s.size();
+        int wordsWindow = words.size() * wordLen;
+        
+        unordered_map<string, int> wordCount;
+        for(int i=0; i<words.size(); i++) {
+            wordCount[words[i]]++;
+        }
+        
+        int i = 0;
+        while(i + wordsWindow <= sLen) {
+            if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
+                res.push_back(i);
+            }
+            i++;
+        }
+        return res;
+    }
+};
+
+
+
+  
