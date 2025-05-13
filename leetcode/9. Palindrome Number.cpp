e65f6f26c64526b7ce55efde7f4a7b3cb084d435
Anshi Aggarwal <52320166+anshi-a2@users.noreply.github.com>
2025-05-13 21:44:37 +0530

    Update 9. Palindrome Number.cpp

--- leetcode/9. Palindrome Number.cpp
+++ leetcode/9. Palindrome Number.cpp
@@ -1,6 +1,27 @@
 // PROBLEM :
 Given an integer x, return true if x is a palindrome, and false otherwise.
 
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+    
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+
+    
 // SOLUTION :
 
 class Solution {
@@ -21,3 +42,29 @@ public:
         return reverse == xcopy;         
     }
 };
+
+
+// SOLUTION 02 (running half the loop) :
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (but not 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+
+        // Only reverse half of the digits
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even and odd length numbers
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+};
+
+
+
